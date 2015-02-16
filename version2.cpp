fd3c463f9ef4eae8344582dc786c6da781336bb4
Roberto Rodriguez <brod6074@users.noreply.github.com>
2015-02-16 08:47:37 -0800

    beginning work on version 2

---
+++ version2.cpp
@@ -0,0 +1,10 @@
+// Programmer:	Roberto Rodriguez
+// Assignment:	Individual Project
+// Class:		CS 408, W15
+//
+// VERSION 2
+
+#include <iostream>
+using namespace std;
+
+static const int SET_MAX = 6;
